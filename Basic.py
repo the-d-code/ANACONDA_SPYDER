8fd047bbbe3407e9e5f818c681069b50b1741667
The D Akbari <diya0904ak@gmail.com>
2023-07-20 14:07:08 +0530

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -4,132 +4,148 @@
 
 // -------------------------------------------
 
-msg print
-msg="welcome to pythone.."
-print (msg)
+        msg print
+        msg="welcome to pythone.."
+        print (msg)
 
 // -------------------------------------------
 
-Input and print with string
-val=int(input("enter a value"))
-print("welcome {}".format(val))
-print(type(val))
+        Input and print with string
+        val=int(input("enter a value"))
+        print("welcome {}".format(val))
+        print(type(val))
 
 
 // -------------------------------------------
 
-        # User define function
-          # declare function
-        def myf(str):
-          # str=print("Welcome to python...")
-          print(str)
-          
-        print("my first python function..")
-           
-          # call myf function
-        myf("Welcome")
-
-
-          Output:
-          
-          my first python function..
-          Welcome
-       // -------------------------------------------
+                # User define function
+                  # declare function
+                def myf(str):
+                  # str=print("Welcome to python...")
+                  print(str)
+                  
+                print("my first python function..")
+                   
+                  # call myf function
+                myf("Welcome")
         
-          # User define function
-          # declare function
-        def myf(str,str1):
-          # str=print("Welcome to python...")
-          print(str,str1)
-          
-        print("my first python function..")
-           
-          # call myf function
-        myf("Welcome","python")
         
-        Output:
-        
-        my first python function..
-        Welcome python
-
-// -------------------------------------------
-        mylist=[10,30,40,20,50,20,40]
-        print(mylist)
-        
-        Output:
-        [10, 30, 40, 20, 50, 20, 40]
-
-        // -------------------------------------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(mylist)
-
-        Output:
-        [10, 30, 40, 20, 50, 20, 40, 'python', 5.5]
-
-        // ---------positive index----------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(mylist[0])
-
-        Output:
-        10
-        
-        // ---------nagetive index----------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(mylist[-2])
-
-        Output:
-        python
-
-       // ---------between index----------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(mylist[2:6])
-
-        Output:
-        [40, 20, 50, 20]
+                  Output:
+                  
+                  my first python function..
+                  Welcome
+               // -------------------------------------------
                 
-        // ---------between index----------------
+                  # User define function
+                  # declare function
+                def myf(str,str1):
+                  # str=print("Welcome to python...")
+                  print(str,str1)
+                  
+                print("my first python function..")
+                   
+                  # call myf function
+                myf("Welcome","python")
+                
+                Output:
+                
+                my first python function..
+                Welcome python
 
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(mylist[2:-2])
-
-       Output:
-        [40, 20, 50, 20, 40]
-
-        // ---------length of list----------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(len(mylist))
-
-       Output:
-        9
-
-        // ---------type of list----------------
-
-        mylist=[10,30,40,20,50,20,40, "python",5.5]
-        print(type(mylist))
-
-       Output:
-       <class 'list'>  
-
-        // ---------sum of list----------------
-
-        mylist=[10,30,40,20,50,20,40]
-        print(type(mylist))
-
-       Output:
-       210
-
-        // ---------max of list----------------
-
-        mylist=[10,30,40,20,50,20,40]
-        print(max(mylist))
-
-       Output:
-       50
+// -------------------------------------------
+                mylist=[10,30,40,20,50,20,40]
+                print(mylist)
+                
+                        Output:
+                        [10, 30, 40, 20, 50, 20, 40]
+                
+                // -------------------------------------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(mylist)
+        
+                        Output:
+                        [10, 30, 40, 20, 50, 20, 40, 'python', 5.5]
+                
+                // ---------positive index----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(mylist[0])
+        
+                        Output:
+                        10
+                
+                // ---------nagetive index----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(mylist[-2])
+        
+                        Output:
+                        python
+                
+               // ---------between index----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(mylist[2:6])
+        
+                        Output:
+                        [40, 20, 50, 20]
+                                
+                // ---------between index----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(mylist[2:-2])
+        
+                       Output:
+                        [40, 20, 50, 20, 40]
+        
+                // ---------length of list----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(len(mylist))
+        
+                       Output:
+                        9
+        
+                // ---------type of list----------------
+        
+                mylist=[10,30,40,20,50,20,40, "python",5.5]
+                print(type(mylist))
+        
+                       Output:
+                       <class 'list'>  
+        
+                // ---------sum of list----------------
+        
+                mylist=[10,30,40,20,50,20,40]
+                print(type(mylist))
+        
+                       Output:
+                       210
+        
+                // ---------max of list----------------
+        
+                mylist=[10,30,40,20,50,20,40]
+                print(max(mylist))
+        
+                       Output:
+                       50
+                
+                // ---------max of list----------------
+                mylist=[10,30,40,20,50,20,40]
+                mylist[0]=100
+                print(mylist)
+        
+                        Output:
+                        [100, 30, 40, 20, 50, 20, 40]
+                
+                // ---------max of list----------------
+                mylist=[10,30,40,20,50,20,40]
+                mylist.remove(50)
+                print(mylist)
+                
+                        Output:
+                        [10, 30, 40, 20, 20, 40]
 
 
 
