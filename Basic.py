7f3b249a359767f453d7afb70d2fd4636248912e
The D Akbari <diya0904ak@gmail.com>
2023-07-20 13:50:36 +0530

    Create Basic.py
    basic data

---
+++ Basic.py
@@ -0,0 +1,57 @@
+# In main file
+# import script1
+# print(script1.sum(1, 3))
+
+// -------------------------------------------
+# msg print
+# msg="welcome to pythone.."
+# print (msg)
+
+// -------------------------------------------
+
+# Input and print with string
+# val=int(input("enter a value"))
+# print("welcome {}".format(val))
+# print(type(val))
+
+
+// -------------------------------------------
+
+        # User define function
+          # declare function
+        def myf(str):
+          # str=print("Welcome to python...")
+          print(str)
+          
+        print("my first python function..")
+           
+          # call myf function
+        myf("Welcome")
+
+
+          Output:
+          
+          my first python function..
+          Welcome
+       // -------------------------------------------
+        
+          # User define function
+          # declare function
+        def myf(str,str1):
+          # str=print("Welcome to python...")
+          print(str,str1)
+          
+        print("my first python function..")
+           
+          # call myf function
+        myf("Welcome","python")
+        
+        Output:
+        
+        my first python function..
+        Welcome python
+
+       // -------------------------------------------
+
+
+ // -------------------------------------------  
