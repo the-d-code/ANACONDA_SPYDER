519023d9a851a0d4cec00c33329ba9d813dc3c36
The D Akbari <diya0904ak@gmail.com>
2023-07-22 13:53:32 +0530

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -1,6 +1,8 @@
-# In main file
-# import script1
-# print(script1.sum(1, 3))
+
+
+
+=====================================LECTURE 1=============================================================================================
+
 
 // -------------------------------------------
 
@@ -147,7 +149,7 @@
                         Output:
                         [10, 30, 40, 20, 20, 40]
 
-
+============= 22/7/23 ========================================== LECTURE 2 =============================================================================================
 
  // ----------Array-----------------  
         
@@ -211,19 +213,70 @@
 
         
                 // ------Replace the item-------------------
+         
+                        tuple1=(1,2,5.0,"Hello")
+                        tuple.append(100)
+                        print(tuple1[-1])
+                        
+                                type(mylist)
+                                Out[20]: list
+                        
+                // ------reverse the item-------------------
+
+                        mylist=[[1,2,3],["Hemali","Devanshi","D"]]
+                        mylist.reverse()
+                        print(mylist)
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                [['Hemali', 'Devanshi', 'D'], [1, 2, 3]]
+
+                // ------sorting the item-------------------
+
+                        mylist=[10,20,30,40,50,60,1000,250]
+                        l1=sorted(mylist)
+                        print(l1)
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                [10, 20, 30, 40, 50, 60, 250, 1000]
+
+                // ------for loop the item-------------------
+
+                        mylist=[10,20,30,40,50,60,1000,250]
+                        for i in mylist:
+                            print(i)
+                                
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                10
+                                20
+                                30
+                                40
+                                50
+                                60
+                                1000
+                                250
+
+        
+                        // ------multi the item-------------------
+
+                        mylist=[10,20,30,40,50,60,1000,250]
+                        for i in mylist:
+                            print(i*2)
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                20
+                                40
+                                60
+                                80
+                                100
+                                120
+                                2000
+                                500
+                // -------------------------
 
 
-
-
-
-                // ------Revome the item-------------------
-
-                // ------Replace the item-------------------
-                // ------Revome the item-------------------
-
-
-                // ------Replace the item-------------------
-                // ------Revome the item-------------------
+        
+                // -------------------------
+                // -------------------------
 
 
 
