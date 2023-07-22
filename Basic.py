f2daac1d703c2e0804d38c3611aebd388d07c5c8
The D Akbari <diya0904ak@gmail.com>
2023-07-22 13:42:47 +0530

    Update Basic.py

--- Basic.py
+++ Basic.py
@@ -149,4 +149,84 @@
 
 
 
- // -------------------------------------------  
+ // ----------Array-----------------  
+        
+                        mylist=[[1,2,3],["Hemali","Devanshi","D"]]
+                        print(mylist[1][1])
+                
+                                runfile('C:/Users/admin/untitled0.py', wdir='C:/Users/admin')
+                                Devanshi
+      
+                // ------Insert the item-------------------
+                                
+                        mylist=[[1,2,3],["Hemali","Devanshi","D"]]
+                        mylist.append("NEW")
+                        print(mylist)
+        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                [[1, 2, 3], ['Hemali', 'Devanshi', 'D'], 'NEW']
+
+                 // ------Insert the item-------------------
+                        
+                        mylist=[[1,2,3],["Hemali","Devanshi","D"]]
+                        mylist.insert(0,100)
+                        print(mylist)
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                [100, [1, 2, 3], ['Hemali', 'Devanshi', 'D']]
+
+                // ------Revome the item-------------------
+
+                        mylist=[[1,2,3],["Hemali","Devanshi","D"]]
+                        mylist.insert(0,100)
+                        list1=mylist
+                        mylist.remove(100)
+                        print(list1)
+
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                [[1, 2, 3], ['Hemali', 'Devanshi', 'D']]
+
+
+// ------TUPPLE-------------------
+
+                        tuple1=(1,2,5.0,"Hello")
+                        print(tuple1)
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                (1, 2, 5.0, 'Hello')
+
+                // ------retrive the item-------------------
+
+                        tuple1=(1,2,5.0,"Hello")
+                        print(tuple1[-1])
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                Hello
+
+                        tuple1=(1,2,5.0,"Hello")
+                        print(tuple1[2])
+                        
+                                runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                5.0
+
+        
+                // ------Replace the item-------------------
+
+
+
+
+
+                // ------Revome the item-------------------
+
+                // ------Replace the item-------------------
+                // ------Revome the item-------------------
+
+
+                // ------Replace the item-------------------
+                // ------Revome the item-------------------
+
+
+
+
+
+
