bd8919d55747d90d629252db3f4a290ad325058e
The D Akbari <diya0904ak@gmail.com>
2023-07-25 14:04:18 +0530

    Update All_In_One.py

--- All_In_One.py
+++ All_In_One.py
@@ -407,17 +407,42 @@
 // ------ Class ------------
 
                 // -----Create class and Print value----------------
+                                   class abc:
+                                       def show():
+                                           print("First python class")
+                                   
+                                   obj=abc()
+                                   abc.show()
+                                   
+                                                 runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                                 First python class
+              
+                                                   
+                         // ---------- class using constructor-------------
+                                   
+                                          class abc:
+                                              def show(self):
+                                                  print("First python class")
+                                          
+                                          obj=abc()
+                                          obj.show()
+              
+                                                        runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
+                                                        First python class
+                                                        
+                // --------Take static input--------------
                      class abc:
-                         def show():
-                             print("First python class")
+                         def __init__(self,name):
+                             self.name=name
+                         def show(self):
+                             print("Welcome",self.name)
                      
-                     obj=abc()
-                     abc.show()
+                     obj=abc("Devanshi")
+                     obj.show()
                      
                                    runfile('B:/ICT3-1/SPYDER/ARRAY.py', wdir='B:/ICT3-1/SPYDER')
-                                   First python class
-                // -------------------------
- 
+                                   Welcome Devanshi
+                                          
                 // -------------------------
 
                 // -------------------------
