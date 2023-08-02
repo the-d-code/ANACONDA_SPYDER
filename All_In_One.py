8da67edd095feb6061766e158ff81295a214a3b2
The D Akbari <diya0904ak@gmail.com>
2023-08-02 11:45:22 +0530

    Update All_In_One.py

--- All_In_One.py
+++ All_In_One.py
@@ -478,9 +478,32 @@
                                    runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
                                    xyz class method...
 
-                // -------------------------
+============= 11/8/23 ========================================== LECTURE 4 =============================================================================================
  
-                // -------------------------
+                // ---run time polymorphism-------------
+
+                     class abc:
+                         def show(self):
+                             print("Welcome")
+                     
+                     class xyz(abc):
+                         def show(self):
+                             print("xyz class method...")
+                             
+                     obj=any
+                     
+                     obj1=abc()
+                     obj=obj1
+                     obj.show()
+                     
+                     obj2=xyz()
+                     obj=obj2
+                     obj.show()
+                     
+                     
+                                   runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
+                                   Welcome
+                                   xyz class method...
 
                 // -------------------------
  
