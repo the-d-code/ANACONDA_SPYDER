a47b4dac5723428fa1134b7b80507fa30562a144
The D Akbari <diya0904ak@gmail.com>
2023-08-02 12:13:59 +0530

    Update Array.py

--- Numpy/Array.py
+++ Numpy/Array.py
@@ -1,1 +1,29 @@
 
+# 1D ARRAY
+
+import numpy as np
+n1=np.array([10,30,50,60,90])
+print(n1)
+
+        runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
+        [10 30 50 60 90]
+
+# ARRAY OF ARRAY / 2D ARRAY
+
+import numpy as np
+n1=np.array([[10,30,50,60,90],[5,15,25,35,45]])
+print(n1)
+
+    runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
+    [[10 30 50 60 90]
+     [ 5 15 25 35 45]]
+
+# SIZE OF ARRAY
+
+import numpy as np
+n1=np.array([[10,30,50,60,90],[5,15,25,35,45]])
+print(n1.shape)
+
+  import numpy as np
+  n1=np.array([[10,30,50,60,90],[5,15,25,35,45]])
+  print(n1.shape)
