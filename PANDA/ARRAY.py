45eca6d665a91dddbf584a268a1226da02dc8b74
The D Akbari <diya0904ak@gmail.com>
2023-08-09 11:55:56 +0530

    Create ARRAY.py

---
+++ PANDA/ARRAY.py
@@ -0,0 +1,26 @@
+# 1D ARRAY SERIES
+
+import pandas as pd
+
+S1=pd.Series([2,.4,6,8,10])
+print(S1)
+
+    runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
+    0     2.0
+    1     0.4
+    2     6.0
+    3     8.0
+    4    10.0
+    dtype: float64
+
+
+import pandas as pd
+
+S1=pd.Series(["PYTHON","PROGRAM"])
+print(S1)
+    
+    runfile('B:/ICT3-1/OTHER STUUF/SPYDER/ARRAY.py', wdir='B:/ICT3-1/OTHER STUUF/SPYDER')
+    0     PYTHON
+    1    PROGRAM
+    dtype: object
+
